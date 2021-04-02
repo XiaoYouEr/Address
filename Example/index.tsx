b0c0adfaa79cc8f174978f8e2a8cb9c34ed5064b
XiaoYouEr <33562582+XiaoYouEr@users.noreply.github.com>
2021-04-02 17:22:02 +0800

    Create index.tsx

---
+++ Example/index.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styles from "./index.less";
+
+export default function IndexPage() {
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState(props.city || "");
+  const [district, setDistrict] = useState(props.district || "");
+  const [streetName, setStreetName] = useState(props.streetName || "");
+  const [addressVisible, setAddressVisible] = useState(false);
+  const handleAddress = (result: { city?: string; district?: string; streetName?: string }) => {
+    setAddress([result.city || "", result.district || "", result.streetName || ""].join(" "));
+    setCity(result.city || "");
+    setDistrict(result.district || "");
+    setStreetName(result.streetName || "");
+  };
+  return (
+    <div>
+      点击选择
+      <div className={styles.selectBox} onClick={() => {setAddressVisible(true)}}>
+        {currentValue}
+      </div>
+      <Address hierarchy={3} getAddress={handleAddress} closeModel={() => {setAddressVisible(false)}} visible={addressVisible}/>
+    </div>
+  );
+}
